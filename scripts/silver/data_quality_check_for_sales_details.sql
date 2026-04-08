8b1a54679c5763c9e756c17c17e6baedfa060aeb
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-08 20:34:55 +0530

    Update data_quality_check_for_sales_details.sql

--- scripts/silver/data_quality_check_for_sales_details.sql
+++ scripts/silver/data_quality_check_for_sales_details.sql
@@ -8,3 +8,30 @@ SELECT
 *
 FROM bronze.crm_sales_details
 WHERE sls_prd_key <> TRIM(sls_prd_key)
+
+--Checking the sls_prd_key relation 
+SELECT TOP (1000) [sls_ord_num]
+      ,REPLACE(TRIM(sls_prd_key),'-','_') sls_prd_key
+      ,[sls_cust_id]
+      ,[sls_order_dt]
+      ,[sls_ship_dt]
+      ,[sls_due_dt]
+      ,[sls_sales]
+      ,[sls_quantity]
+      ,[sls_price]
+  FROM [DataWarehouse].[bronze].[crm_sales_details]
+  WHERE REPLACE(TRIM(sls_prd_key),'-','_') NOT IN (SELECT prd_key 
+FROM silver.crm_prd_info)
+
+SELECT TOP (1000) [sls_ord_num]
+      ,REPLACE(TRIM(sls_prd_key),'-','_') sls_prd_key
+      ,[sls_cust_id]
+      ,[sls_order_dt]
+      ,[sls_ship_dt]
+      ,[sls_due_dt]
+      ,[sls_sales]
+      ,[sls_quantity]
+      ,[sls_price]
+  FROM [DataWarehouse].[bronze].[crm_sales_details]
+  WHERE sls_cust_id NOT IN (SELECT cst_id 
+FROM silver.crm_cust_info)
