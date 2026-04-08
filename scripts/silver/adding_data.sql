35d9aac533244a9299eda67b49956cbd0c3b4fff
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-08 21:28:43 +0530

    Insert data into silver.crm_sales_details table

--- scripts/silver/adding_data.sql
+++ scripts/silver/adding_data.sql
@@ -62,3 +62,44 @@ SELECT
       ,CAST(prd_start_dt AS DATE) prd_start_dt
       ,CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) prd_end_dt
   FROM bronze.[crm_prd_info]
+
+
+
+TRUNCATE TABLE silver.crm_sales_details;
+
+INSERT INTO
+silver.crm_sales_details
+(
+	sls_ord_num,
+	sls_cust_id,
+	sls_prd_key,
+	sls_order_dt,
+	sls_due_dt,
+	sls_ship_dt,
+	sls_sales,
+	sls_price,
+	sls_quantity	
+)
+SELECT
+sls_ord_num,
+sls_cust_id,
+REPLACE(TRIM(sls_prd_key),'-','_') sls_prd_key,
+CASE WHEN sls_order_dt <=0 OR LEN(sls_order_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END sls_order_dt,
+CASE WHEN sls_due_dt <=0 OR LEN(sls_due_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END sls_due_dt,
+CASE WHEN sls_ship_dt <=0 OR LEN(sls_ship_dt) != 8 THEN NULL
+	ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END sls_ship_dt,
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity*ABS(sls_price)
+	THEN sls_quantity*ABS(sls_price)
+	ELSE sls_sales
+END sls_sales,
+CASE WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales/NULLIF(sls_quantity,0)
+	ELSE sls_price
+END sls_price,
+sls_quantity
+FROM
+bronze.crm_sales_details
