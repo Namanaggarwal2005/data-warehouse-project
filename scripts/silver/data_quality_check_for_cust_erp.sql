67e6a6b124c9dacad8442ad6fc033261f13d5db8
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-08 22:01:24 +0530

    Update data_quality_check_for_cust_erp.sql

--- scripts/silver/data_quality_check_for_cust_erp.sql
+++ scripts/silver/data_quality_check_for_cust_erp.sql
@@ -17,4 +17,17 @@ bdate
 FROM bronze.erp_cust_az12
 WHERE bdate > GETDATE();
 
+--gen
+SELECT DISTINCT
+gen
+FROM
+(
+SELECT
+CASE WHEN UPPER(TRIM(gen)) = 'M' THEN 'Male'
+	WHEN UPPER(TRIM(gen)) = 'F' THEN 'Female'
+	WHEN TRIM(gen) = '' THEN 'n/a'
+	WHEN gen IS NULL THEN 'n/a'
+	ELSE TRIM(gen)
+END gen
+FROM bronze.erp_cust_az12)t
 
