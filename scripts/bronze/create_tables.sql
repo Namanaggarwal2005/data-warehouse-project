004949d2fd5089f93690116b790bcbb35ca4ad04
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-08 11:13:34 +0530

    Create multiple tables in bronze schema
    This script creates multiple tables in the bronze schema, including customer, product, sales details, and various ERP-related tables. Each table is created after dropping it if it already exists.

---
+++ scripts/bronze/create_tables.sql
@@ -0,0 +1,72 @@
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info(
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(1),
+	cst_gndr NVARCHAR(1),
+	cst_create_date VARCHAR(50)
+);
+GO;
+IF OBJECT_ID('bronze.crm_prod_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prod_info;
+CREATE TABLE bronze.crm_prod_info(
+	prod_id INT,
+	prod_key NVARCHAR(50),
+	prod_nm NVARCHAR(50),
+	prod_cost INT,
+	prod_line NVARCHAR(10),
+	prd_start_dt DATE,
+	prd_end_dt DATE
+)
+
+GO;
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key	NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt	DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price FLOAT
+)
+	
+GO
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
