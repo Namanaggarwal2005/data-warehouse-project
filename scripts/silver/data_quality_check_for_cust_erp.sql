01c3fdb7c8b4fa0f99e9c1e804ef83a1c7eb2d0e
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-08 21:45:56 +0530

    Create data_quality_check_for_cust_erp.sql
    Add SQL script for data quality checks on customer ERP data.

---
+++ scripts/silver/data_quality_check_for_cust_erp.sql
@@ -0,0 +1,12 @@
+-- Relationship checking
+SELECT TOP 10
+CASE WHEN cid LIKE 'NAS%' THEN REPLACE(cid,'NAS','') 
+	ELSE cid
+END cid
+FROM 
+bronze.erp_cust_az12;
+
+
+SELECT TOP 10
+cst_key
+FROM silver.crm_cust_info
