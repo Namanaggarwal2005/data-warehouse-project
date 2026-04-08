d776e8085311045439eff32df3e4c8d25fb112e4
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-08 14:31:32 +0530

    Add data insertion logic for crm_prod_info table
    Added SQL commands to truncate and insert data into silver.crm_prod_info from bronze.crm_prd_info.

--- scripts/silver/adding_data.sql
+++ scripts/silver/adding_data.sql
@@ -32,3 +32,33 @@ INSERT INTO silver.crm_cust_info
 		WHERE cst_id IS NOT NULL
 	)t
 	WHERE flag = 1;
+
+
+
+TRUNCATE TABLE silver.crm_prod_info;
+
+INSERT INTO silver.crm_prod_info (
+	prd_id,
+	cat_id ,
+    prd_key ,
+	prd_nm,
+	prd_cost ,
+	prd_line ,
+	prd_start_dt ,
+	prd_end_dt 
+)
+SELECT
+       prd_id
+      ,REPLACE(SUBSTRING(prd_key,1,5),'-','_') cat_id
+      ,REPLACE(SUBSTRING(prd_key,7),'-','_') prd_key
+      ,TRIM(prd_nm) prd_nm
+      ,ISNULL(prd_cost,0) prd_cost
+      ,CASE WHEN prd_line='M' THEN 'Mountain'
+            WHEN prd_line='R' THEN 'Road'
+            WHEN prd_line='S'THEN 'Other Sale'
+            WHEN prd_line='T' THEN 'Travel'
+            ELSE 'n/a'
+        END prd_line
+      ,CAST(prd_start_dt AS DATE) prd_start_dt
+      ,CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) prd_end_dt
+  FROM bronze.[crm_prd_info]
