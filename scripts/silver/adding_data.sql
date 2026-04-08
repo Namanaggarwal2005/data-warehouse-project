1a492bba60e188c29c33e07c7497b85fd2032878
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-08 22:39:57 +0530

    Add data to silver.erp_loc_a101 with country mapping
    Truncate silver.erp_loc_a101 and insert cleaned country data.

--- scripts/silver/adding_data.sql
+++ scripts/silver/adding_data.sql
@@ -128,3 +128,18 @@ CASE WHEN UPPER(TRIM(gen)) = 'M' THEN 'Male'
 	ELSE TRIM(gen)
 END gen
 FROM bronze.erp_cust_az12
+
+TRUNCATE TABLE silver.erp_loc_a101;
+
+INSERT INTO 
+silver.erp_loc_a101
+(cid,cntry)
+SELECT
+REPLACE(cid,'-','') cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	ELSE TRIM(cntry)
+END cntry
+FROM 
+bronze.erp_loc_a101
