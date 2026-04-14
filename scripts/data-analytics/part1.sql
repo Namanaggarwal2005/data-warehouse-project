8701f243f8592fb5661b5a2e936a1238fd9c4358
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-14 13:35:03 +0530

    Update and rename part1 to part1.sql

---
+++ scripts/data-analytics/part1.sql
@@ -0,0 +1,21 @@
+-- Countries from where our customers come from
+SELECT DISTINCT 
+country
+FROM gold.dim_customers
+
+--Different Products we SELL
+SELECT DISTINCT
+category,
+subcategory,
+product_name
+FROM gold.dim_products
+ORDER BY 1,2,3
+
+--Date of first and last order
+SELECT
+	MIN(order_date) AS first_order,
+	MAX(order_date) AS latest_order,
+	DATEDIFF(year,MIN(order_date),MAX(order_date)) AS diff
+from
+gold.fact_sales
+
