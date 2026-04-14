f1f7957e24145727cfa2ff43f5526b5ad170c3bc
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-14 11:16:15 +0530

    Add SQL query to retrieve sales details with products

--- scripts/golden/product_view_creation.sql
+++ scripts/golden/product_view_creation.sql
@@ -34,3 +34,21 @@ ON p_info.cat_id = p_cat.id
 WHERE p_info.prd_end_dt IS NULL
 ) t
 GROUP BY prd_id HAVING COUNT(*) > 1;
+
+SELECT 
+  sd.sls_ord_num,
+  pr.product_key,
+  sd.sls_prd_key,
+  sd.sls_cust_id,
+  sd.sls_order_dt,
+  sd.sls_due_dt,
+  sd.sls_ship_dt,
+  sd.sls_sales,
+  sd.sls_quantity,
+  sd.sls_price
+FROM 
+silver.crm_sales_details AS sd
+LEFT JOIN 
+gold.dim_products AS pr
+ON sd.sls_prd_key = pr.product_number
+ORDER BY product_key
