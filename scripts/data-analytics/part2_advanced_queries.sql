d800ae87453391571f445a258f96f498ea6a7ccc
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-15 20:31:51 +0530

    Add cumulative sales analysis query
    Added cumulative sales analysis query to calculate running total by year and month.

--- scripts/data-analytics/part2_advanced_queries.sql
+++ scripts/data-analytics/part2_advanced_queries.sql
@@ -19,4 +19,18 @@ WHERE order_date IS NOT NULL
 GROUP BY MONTH(order_date)
 ORDER BY SUM(sales_amount)  DESC
 
---
+--Cummulative Analysis
+SELECT
+*,
+SUM(total_sales) OVER(PARTITION BY order_year ORDER BY order_year, order_month) AS running_total
+FROM
+(
+	SELECT
+		YEAR(order_date) AS order_year,
+		MONTH(order_date) AS order_month,
+		SUM(sales_amount) as total_sales
+	FROM
+	gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY YEAR(order_date), MONTH(order_date)
+)t
