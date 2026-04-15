381117e0b0ebe9cbe9f3d416dd98b85144f8d142
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-15 21:01:00 +0530

    Implement part-to-whole analysis for category sales
    Added a part-to-whole analysis for category sales with percentage calculation.

--- scripts/data-analytics/part2_advanced_queries.sql
+++ scripts/data-analytics/part2_advanced_queries.sql
@@ -72,3 +72,21 @@ SELECT
     END AS py_change
 FROM yearly_product_sales
 ORDER BY product_name, order_year;
+
+--Part to whole analysis (Pie Chart) // Bikes perfomed the best
+WITH category_sales AS (
+    SELECT
+        p.category,
+        SUM(f.sales_amount) AS total_sales
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+        ON p.product_key = f.product_key
+    GROUP BY p.category
+)
+SELECT
+    category,
+    total_sales,
+    SUM(total_sales) OVER () AS overall_sales,
+    ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC;
