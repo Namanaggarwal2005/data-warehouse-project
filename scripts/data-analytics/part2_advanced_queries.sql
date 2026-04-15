a88670d7a363e17c827bed5bc2912c415a1d38e0
Naman Aggarwal <namanaggarwal98124@gmail.com>
2026-04-15 20:18:24 +0530

    Update part2_advanced_queries.sql

--- scripts/data-analytics/part2_advanced_queries.sql
+++ scripts/data-analytics/part2_advanced_queries.sql
@@ -1,9 +1,22 @@
 --Change over time
 SELECT
   YEAR(order_date) AS orders_year,
-  SUM(sales_amount) as total_sales
+  SUM(sales_amount) as total_sales,
+  COUNT(DISTINCT customer_key) as customers
 FROM
   gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY YEAR(order_date)
-ORDER BY YEAR(order_date) DESC
+ORDER BY SUM(sales_amount)  DESC
+
+
+SELECT
+  MONTH(order_date) AS orders_month,
+  SUM(sales_amount) as total_sales
+FROM
+  gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY MONTH(order_date)
+ORDER BY SUM(sales_amount)  DESC
+
+--
